233acb495c4d27f90b821ebc6f531b1df68836bc
lynnagneta <lynnetteagneta@live.com>
2022-04-20 14:34:45 -0700

    Task for 4-print_rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * Print_rev - function that prints a string, in reverse,
+ * Print_rev - a function that prints a string, in reverse,
  * followed by a new line.
  * @s: an input string
  * Return: Nothing
