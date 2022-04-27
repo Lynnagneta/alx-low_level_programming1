81d6c7f6ed3b148255d1c2d28cb6705100e34855
Lynnagneta <lynnetteagneta@live.com>
2022-04-27 01:52:56 -0700

    My project 0

--- 0-strcat.c
+++ 0-strcat.c
@@ -8,17 +8,14 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int i, c;
+	char *temp = dest;
 
-	for (i = 0; dest[i] != '\0'; i++)
-		;
+	while (*dest)
+		dest++;
 
-	for (c = 0; src[c] != '\0'; c++)
-	{
-		dest[i] = src[c];
-		i++;
-	}
+	while (*src)
+		*dest++ = *src++;
 
-	dest[i] = '\0';
-	return (dest);
+	*dest = '\0';
+	return (temp);
 }
